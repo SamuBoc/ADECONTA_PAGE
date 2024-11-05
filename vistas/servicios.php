0371b2d33341e12656c641d55bed232f0ddebe73
samuBoc <samuelandresbonilla@hotmail.com>
2024-11-05 15:46:57 -0500

    update: banner us

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -1,1 +1,75 @@
-ESTA ES LA PAGINA DE SERVICIOS
+<style>
+
+/* US SECTION */
+
+.main__wrapper-us{
+    background-image: linear-gradient(rgba(9, 144, 25, 0.594) , rgba(9, 144, 24, 0.594)), url(./img/revistas_banner.png);
+    min-height: auto;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    color: rgb(255, 255, 255, 1);
+    display: flex;
+    justify-content: center; /* Centra el contenido verticalmente */
+}
+
+.main__wrapper-us p{
+    font-size: 2.5vh;
+    padding: 6vh;
+    font-weight: 317;
+}
+
+.main__wrapper-us h1{
+    font-size:6.5vh;
+    font-weight: 1117;
+    text-align: center; /* Asegura que el texto del h1 esté centrado */
+}
+
+@media screen and (min-width: 1000px) {
+    .main__wrapper-us{
+        background-size: 170%;
+        background-position: left;
+        justify-content: flex-start;
+        align-items: flex-start;
+        background-image: linear-gradient(rgba(9, 144, 24, 0.3) , rgba(9, 144, 24, 0.3)), url(./img/revistas_banner.png);
+
+    }
+
+    .main__wrapper-us h1, .main__wrapper-us p{
+        text-align: start;
+        padding-top: 15vh;
+        padding-left: 10vh;
+        max-width: 30vw;
+        line-height: 3.5vh;
+    }
+
+    .main__wrapper-us h1{
+        font-size:10vh;
+    }
+
+    .main__wrapper-us p{
+        padding-top: 20vh;
+        padding-bottom: 10vh;
+    }
+}
+
+/* END US SECTION */
+
+</style>
+
+<!-- START US  -->
+    
+<section>
+            <!-- START NOSOTROS  -->
+                
+            <div class="main__wrapper-us">
+                    <div class="main__wrapper__us-text">
+                        <h1>Nosotros</h1>
+                        <p>LA ASOCIACIÓN DE CONTADORES PÚBLICOS DEL VALLE DEL CAUCA, ADECONTA, nace el 06 de noviembre de 1956, dos meses después de la legalización de la Contaduría Pública en Colombia y de la creación de la Junta Central de Contadores. <br> Hoy en día Adeconta continúa ligada al principio que le dio origen: La profesionalización constante de los Contadores Públicos. Un objetivo primario que se desarrolla en los servicios brindados por la Asociación y se preserva en la calidad académica de sus miembros Adeconta hace parte del Comité Nacional Gremial y permanece en contacto permanente con asociaciones de contadores de diferentes regiones del país. <br> Adeconta suma a su experiencia sesenta y ocho años de espíritu renovador, consecuente con los requerimientos actuales de la profesión Contable y su globalización y avances tecnológicos.</p>
+                    </div>
+                </div>
+    
+                <!-- END NOSOTROS -->
+        </section>
+    
+        <!-- END US -->
